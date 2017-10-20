d61cead809e605732a4eaaf3fa53ca84ed5c0e6b
Vladimir Abramichev <vladimirab@backbase.com>
2017-10-20 14:41:55 +0200

    Referencing: Adding all more cases

--- General.playground/Pages/Referencing.xcplaygroundpage/Contents.swift
+++ General.playground/Pages/Referencing.xcplaygroundpage/Contents.swift
@@ -4,61 +4,119 @@ import Foundation
 
 var str = "Hello, playground"
 
-protocol AProtocol: class, CustomStringConvertible {
-    var value: String {get}
+protocol ReferrenceProtocol: class, CustomStringConvertible {
+    var reference: ReferrenceProtocol? { get set }
+    var value: String { get }
 }
 
-protocol BProtocol: class, CustomStringConvertible {
-    var value: String {get}
+extension ReferrenceProtocol  {
+    var description: String {
+        return "Ref contains \( reference?.value ?? "Nil")"
+    }
 }
 
-
-class A : AProtocol {
-    weak var b: BProtocol?
+class StrongReferenced: ReferrenceProtocol {
+    var reference: ReferrenceProtocol?
     let value: String
     
     init(value: String) {
         self.value = value
-    }
-    
-    var description: String {
-        return b?.value ?? "Nil"
+        print("\(value) init")
     }
     
     deinit {
-        print("A destroyed")
+        print("\(value) destroyed")
     }
 }
 
-class B : BProtocol {
-    weak var a: AProtocol?
+class WeakReferenced : ReferrenceProtocol {
+    weak var reference: ReferrenceProtocol?
     let value: String
     
     init(value: String) {
         self.value = value
-    }
-    
-    var description: String {
-        return a?.value ?? "Nil"
+        print("\(value) init")
     }
     
     deinit {
-        print("B destroyed")
+        print("\(value) destroyed")
     }
 }
 
-let a = A(value: "A")
-
-func someScope() {
-    a.b = B(value: "B")
+func BidirectionalStoredScope(_ left: ReferrenceProtocol, _ right: ReferrenceProtocol) {
+    left.reference = right
+    right.reference = left
 }
 
-print(a)
+func MonodirectionalStoredScope(_ left: ReferrenceProtocol, _ right: ReferrenceProtocol) {
+    left.reference = right
+}
 
-someScope()
-print(a)
+var a : StrongReferenced?
+var b : WeakReferenced?
+var c : StrongReferenced?
 
+func checkingStrongReferencedStoredInWeakReferenced() {
+    print("\n -- Testing StrongReferenced Stored in WeakReferenced :")
+    a = StrongReferenced(value: "StrongReferenced")
+    BidirectionalStoredScope(a!, WeakReferenced(value: "WeakReferenced"))
+    
+    print("If kill \(a!.value):")
+    a = nil
+}
+
+func checkingStrongReferencedNotStoredInWeakReferenced() {
+    print("\n -- Testing StrongReferenced NOT Stored in WeakReferenced :")
+    a = StrongReferenced(value: "StrongReferenced")
+    MonodirectionalStoredScope(a!, WeakReferenced(value: "WeakReferenced"))
+    
+    print("If kill \(a!.value):")
+    a = nil
+}
+
+func checkingWeakReferencedStoredInStrongReferenced() {
+    print("\n -- Testing WeakReferenced Stored in StrongReferenced :")
+    b = WeakReferenced(value: "WeakReferenced")
+    BidirectionalStoredScope(StrongReferenced(value: "StrongReferenced"), b!)
+    
+    print("If kill \(b!.value):")
+    b = nil
+}
+
+func checkingWeakReferencedNotStoredInStrongReferenced() {
+    print("\n -- Testing StrongReferenced NOT Stored in WeakReferenced :")
+    b = WeakReferenced(value: "WeakReferenced")
+    MonodirectionalStoredScope(StrongReferenced(value: "StrongReferenced"), b!)
+    
+    print("If kill \(b!.value):")
+    b = nil
+}
+
+func checkingStrongReferencedStoredInStrongReferenced() {
+    print("\n -- Testing StrongReferenced Stored in StrongReferenced :")
+    c = StrongReferenced(value: "StrongReferenced")
+    BidirectionalStoredScope(StrongReferenced(value: "StrongReferenced"), c!)
+    
+    print("If kill \(c!.value):")
+    c = nil
+}
+
+func checkingStrongReferencedNotStoredInStrongReferenced() {
+    print("\n -- Testing StrongReferenced NOT Stored in StrongReferenced :")
+    c = StrongReferenced(value: "Second StrongReferenced")
+    MonodirectionalStoredScope(StrongReferenced(value: "StrongReferenced"), c!)
+    
+    print("If kill \(c!.value):")
+    c = nil
+}
 
 //: [Next](@next)
 
 print("✅")
+
+checkingStrongReferencedStoredInWeakReferenced()
+checkingStrongReferencedNotStoredInWeakReferenced()
+checkingWeakReferencedStoredInStrongReferenced()
+checkingWeakReferencedNotStoredInStrongReferenced()
+checkingStrongReferencedStoredInStrongReferenced()
+checkingStrongReferencedNotStoredInStrongReferenced()
