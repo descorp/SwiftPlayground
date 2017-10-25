e6cbdc0f8aab0a0f33d87e115478795ffaf31f72
Vladimir Abramichev <vladimirab@backbase.com>
2017-10-25 15:41:29 +0200

    Referencing:  Cleanup code + adding case of storing the reference via third party temporary reference

--- General.playground/Pages/Referencing.xcplaygroundpage/Contents.swift
+++ General.playground/Pages/Referencing.xcplaygroundpage/Contents.swift
@@ -43,6 +43,10 @@ class WeakReferenced : ReferrenceProtocol {
     }
 }
 
+// MARK: references
+
+var globalStrong : ReferrenceProtocol!
+
 func BidirectionalStoredScope(_ left: ReferrenceProtocol, _ right: ReferrenceProtocol) {
     left.reference = right
     right.reference = left
@@ -52,62 +56,106 @@ func MonodirectionalStoredScope(_ left: ReferrenceProtocol, _ right: ReferrenceP
     left.reference = right
 }
 
-var a : StrongReferenced?
-var b : WeakReferenced?
-var c : StrongReferenced?
+// MARK: weak references
+
+weak var globalWeak : ReferrenceProtocol?
+
+func StoredViaTemporaryScope(_ left: ReferrenceProtocol, _ right: @autoclosure () -> ReferrenceProtocol) {
+    globalWeak = right()
+    left.reference = globalWeak
+    globalWeak?.reference = left
+}
+
+// MARK: experiments
 
 func checkingStrongReferencedStoredInWeakReferenced() {
     print("\n -- Testing StrongReferenced Stored in WeakReferenced :")
-    a = StrongReferenced(value: "StrongReferenced")
-    BidirectionalStoredScope(a!, WeakReferenced(value: "WeakReferenced"))
+    globalStrong = StrongReferenced(value: "StrongReferenced")
+    BidirectionalStoredScope(globalStrong!, WeakReferenced(value: "WeakReferenced"))
     
-    print("If kill \(a!.value):")
-    a = nil
+    print("If kill \(globalStrong!.value):")
+    globalStrong = nil
 }
 
 func checkingStrongReferencedNotStoredInWeakReferenced() {
     print("\n -- Testing StrongReferenced NOT Stored in WeakReferenced :")
-    a = StrongReferenced(value: "StrongReferenced")
-    MonodirectionalStoredScope(a!, WeakReferenced(value: "WeakReferenced"))
+    globalStrong = StrongReferenced(value: "StrongReferenced")
+    MonodirectionalStoredScope(globalStrong!, WeakReferenced(value: "WeakReferenced"))
     
-    print("If kill \(a!.value):")
-    a = nil
+    print("If kill \(globalStrong!.value):")
+    globalStrong = nil
 }
 
 func checkingWeakReferencedStoredInStrongReferenced() {
     print("\n -- Testing WeakReferenced Stored in StrongReferenced :")
-    b = WeakReferenced(value: "WeakReferenced")
-    BidirectionalStoredScope(StrongReferenced(value: "StrongReferenced"), b!)
+    globalStrong = WeakReferenced(value: "WeakReferenced")
+    BidirectionalStoredScope(StrongReferenced(value: "StrongReferenced"), globalStrong!)
     
-    print("If kill \(b!.value):")
-    b = nil
+    print("If kill \(globalStrong!.value):")
+    globalStrong = nil
 }
 
 func checkingWeakReferencedNotStoredInStrongReferenced() {
     print("\n -- Testing StrongReferenced NOT Stored in WeakReferenced :")
-    b = WeakReferenced(value: "WeakReferenced")
-    MonodirectionalStoredScope(StrongReferenced(value: "StrongReferenced"), b!)
+    globalStrong = WeakReferenced(value: "WeakReferenced")
+    MonodirectionalStoredScope(StrongReferenced(value: "StrongReferenced"), globalStrong!)
     
-    print("If kill \(b!.value):")
-    b = nil
+    print("If kill \(globalStrong!.value):")
+    globalStrong = nil
 }
 
 func checkingStrongReferencedStoredInStrongReferenced() {
     print("\n -- Testing StrongReferenced Stored in StrongReferenced :")
-    c = StrongReferenced(value: "StrongReferenced")
-    BidirectionalStoredScope(StrongReferenced(value: "StrongReferenced"), c!)
+    globalStrong = StrongReferenced(value: "StrongReferenced")
+    BidirectionalStoredScope(StrongReferenced(value: "Second StrongReferenced"), globalStrong!)
     
-    print("If kill \(c!.value):")
-    c = nil
+    print("If kill \(globalStrong!.value):")
+    globalStrong = nil
 }
 
 func checkingStrongReferencedNotStoredInStrongReferenced() {
     print("\n -- Testing StrongReferenced NOT Stored in StrongReferenced :")
-    c = StrongReferenced(value: "Second StrongReferenced")
-    MonodirectionalStoredScope(StrongReferenced(value: "StrongReferenced"), c!)
+    globalStrong = StrongReferenced(value: "StrongReferenced")
+    MonodirectionalStoredScope(StrongReferenced(value: "Second StrongReferenced"), globalStrong)
     
-    print("If kill \(c!.value):")
-    c = nil
+    print("If kill \(globalStrong.value):")
+    globalStrong = nil
+}
+
+func checkingStrongReferencedStoredViaTempInStrongReferenced() {
+    print("\n -- Testing StrongReferenced via temp reference stored in StrongReferenced :")
+    globalStrong = StrongReferenced(value: "StrongReferenced")
+    StoredViaTemporaryScope(StrongReferenced(value: "Second StrongReferenced"), globalStrong)
+    
+    print("If kill \(globalStrong.value):")
+    globalStrong = nil
+}
+
+func checkingStrongReferencedStoredViaTempInWeakReferenced() {
+    print("\n -- Testing WeakReferenced via temp reference stored in StrongReferenced :")
+    globalStrong = StrongReferenced(value: "StrongReferenced")
+    StoredViaTemporaryScope(WeakReferenced(value: "WeakReferenced"), globalStrong)
+    
+    print("If kill \(globalStrong.value):")
+    globalStrong = nil
+}
+
+func checkingWeakReferencedStoredViaTempInStrongReferenced() {
+    print("\n -- Testing StrongReferenced via temp reference stored in StrongReferenced :")
+    globalStrong = StrongReferenced(value: "StrongReferenced")
+    StoredViaTemporaryScope(globalStrong, StrongReferenced(value: "Second StrongReferenced"))
+    
+    print("If kill \(globalStrong.value):")
+    globalStrong = nil
+}
+
+func checkingWeakReferencedStoredViaTempInWeakReferenced() {
+    print("\n -- Testing WeakReferenced via temp reference stored in StrongReferenced :")
+    globalStrong = WeakReferenced(value: "WeakReferenced")
+    StoredViaTemporaryScope(globalStrong, WeakReferenced(value: "WeakReferenced"))
+    
+    print("If kill \(globalStrong.value):")
+    globalStrong = nil
 }
 
 //: [Next](@next)
@@ -120,3 +168,5 @@ checkingWeakReferencedStoredInStrongReferenced()
 checkingWeakReferencedNotStoredInStrongReferenced()
 checkingStrongReferencedStoredInStrongReferenced()
 checkingStrongReferencedNotStoredInStrongReferenced()
+checkingStrongReferencedStoredViaTempInStrongReferenced()
+checkingStrongReferencedStoredViaTempInWeakReferenced()
