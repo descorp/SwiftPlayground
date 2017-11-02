2d96b7292e7cea12827ec45f820a3a2f1122f2a7
Vladimir Abramichev <vladimirab@backbase.com>
2017-11-02 16:31:06 +0100

    Enums: adding condition for array filtering

--- General.playground/Pages/Enums.xcplaygroundpage/Contents.swift
+++ General.playground/Pages/Enums.xcplaygroundpage/Contents.swift
@@ -6,15 +6,31 @@ var str = "Hello, playground"
 
 //: [Next](@next)
 
-
 enum Answer {
     case yes
     case no
     case maybe(percent: Float)
 }
 
-let survey: [Answer] = [.yes, .no, .maybe(percent: 0.25), .maybe(percent: 0.5), .maybe(percent: 0.80), .maybe(percent: 0.9)]
+let survey: [Answer] = [.yes,
+                        .no,
+                        .maybe(percent: 0.25),
+                        .maybe(percent: 0.5),
+                        .maybe(percent: 0.80),
+                        .maybe(percent: 0.9)]
 
-let pro = survey.map( x in x == .yes ) //( case .yes || (case let .maybe(percent) where percent > 0.5)).count
+let pro = survey.filter {
+    switch $0 {
+    case .yes:
+        return true
+    case let .maybe(percent) where percent > 0.5:
+        return true
+    default:
+        return false
+    }
+}
+
+print(" Pro: \(pro.count) / Con: \(survey.count - pro.count)")
+
 
 print("✅")
