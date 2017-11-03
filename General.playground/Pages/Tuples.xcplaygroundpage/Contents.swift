cba63886e9436bcc3025e1a4acf84ff893d30d46
Vladimir Abramichev <vladimir.abramichev@mail.ru>
2017-11-03 16:40:26 +0100

    Tuples: fizzbuzz example

--- General.playground/Pages/Tuples.xcplaygroundpage/Contents.swift
+++ General.playground/Pages/Tuples.xcplaygroundpage/Contents.swift
@@ -47,4 +47,18 @@ check("a", nil)
 check("A", "b")
 
 
+// Simple fizzbuzz on Swift
+func fizzbuzz(number: Int) -> String {
+    switch (number % 3 == 0, number % 5 == 0) {
+    case (true, false):
+        return "Fizz"
+    case (false, true):
+        return "Buzz"
+    case (true, true):
+        return "FizzBuzz"
+    case (false, false):
+        return String(number)
+    }
+}
+
 print("✅")
